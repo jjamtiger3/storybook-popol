7043b0cef9eb7aef865fe7e1d10d11159a45cadd
서인석 <mr_seois@humuson.com>
2024-11-12 19:15:00 +0900

    fix: test component추가

---
+++ taskbox/src/components/CheckBox.jsx
@@ -0,0 +1,129 @@
+import React, { forwardRef, useImperativeHandle, useState } from 'react'; 
+import styled from 'styled-components';
+
+const CheckboxContainer = styled.div`
+    display: inline-flex; 
+    align-items: center;
+    input {
+        &:checked {
+            accent-color: #10A46E;
+        }
+    }
+    &.red {
+        input {
+            &:checked {
+                accent-color: #ff0000;
+            }
+            &:hover {
+              opacity: 0.8;
+            }
+        }
+    }
+    &.blue {
+        input {
+            &:checked {
+                accent-color: #0000ff;
+            }
+            &:hover {
+              opacity: 0.8;
+            }
+        }
+    }
+    &.readonly {
+        input {
+            background: #c8c8c8;
+            cursor: default;
+            border-color: #c8c8c8;
+            &:hover {
+                opacity: 1;
+            }
+        }
+        label {
+            color: #c8c8c8;
+            cursor: default;
+            &:hover {
+              opacity: 1;
+            }
+        }
+    }
+`;
+
+const CheckboxInput = styled.input`
+    margin-right: ${props => props.labelPosition === 'right' ? '4px' : '0'}; 
+    margin-left: ${props => props.labelPosition === 'right' ? '0' : '4px'};
+`;
+
+const CheckboxLabel = styled.label`
+    margin: 0;
+    background-color: ${props => props.customcss?.style?.backgroundColor};
+    color: ${props => props.customcss?.style?.color};
+    padding: ${props => props.customcss?.style?.padding};
+    height: ${props => props.customcss?.style?.height};
+    line-height: ${props => props.customcss?.style?.height};
+    display: ${props => props.customcss?.style?.display};
+    align-items: ${props => props.customcss?.style?.alignItems};
+    &.emphasis {
+        font-weight: 700;
+    }
+`;
+
+const CheckBox = forwardRef(({ label, id = 'id', labelPosition = 'right', onChange, ...props }, ref) => { 
+    const [checked, setChecked] = useState(props.checked || false);
+    const checkboxId = `checkbox-${id}`;
+    const handleChange = (evt) => {
+        if (props.readOnly) {
+            evt.preventDefault();
+            evt.stopPropagation();
+            return;
+        }
+        setChecked(!checked);
+        if (onChange) {
+            onChange(evt, !checked);
+        }
+    }
+    const setCheckedState = (checked) => {
+        setChecked(checked);
+    }
+    useImperativeHandle(ref, () => ({
+        setCheckedState
+    }));
+
+    return (
+        <CheckboxContainer
+            className={[
+                props.theme, 
+                props.disabled ? 'disabled' : '',
+                props.readOnly ? 'readonly' : '',
+                ...props.classList || []].join(' ')}
+            > 
+            {
+                label && labelPosition === 'left' && 
+                    <CheckboxLabel 
+                        customcss={props.customLabel} 
+                        htmlFor={checkboxId} 
+                        className={[props.emphasisLabel && 'emphasis'].join(' ')}
+                    >
+                        {label}
+                    </CheckboxLabel>
+            } 
+            <CheckboxInput 
+                id={checkboxId} 
+                disabled={props.disabled}
+                checked={checked}
+                onChange={handleChange} 
+                labelPosition={labelPosition} 
+            />
+            {
+                label && labelPosition === 'right' && 
+                    <CheckboxLabel 
+                        className={[props.emphasisLabel && 'emphasis'].join(' ')} 
+                        customcss={props.customLabel} 
+                        htmlFor={checkboxId}
+                    >
+                        {label}
+                    </CheckboxLabel>
+            } 
+        </CheckboxContainer>
+    ) 
+})
+export default CheckBox;
---
+++ taskbox/src/components/CheckBox.stories.jsx
@@ -0,0 +1,19 @@
+import { fn } from "@storybook/test";
+
+import CheckBox from './CheckBox';
+
+export default {
+  component: CheckBox,
+  title: 'CheckBox',
+  tags: ['autodocs'],
+  //👇 "Data"로 끝나는 export들은 스토리가 아닙니다.
+  excludeStories: /.*Data$/,
+  args: {
+  },
+};
+
+export const Default = {
+  args: {
+    label: '체크박스'
+  },
+};
