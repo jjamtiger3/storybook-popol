2a8d701f83fd7a13808b345c02f2f587fadaa74a
서인석 <mr_seois@humuson.com>
2024-11-12 19:09:40 +0900

    fix: 수정

--- taskbox/.storybook/preview.js
+++ taskbox/.storybook/preview.js
@@ -1,6 +1,3 @@
-
-import '../src/index.css';
-
 //👇 Configures Storybook to log the actions( onArchiveTask and onPinTask ) in the UI.
 /** @type { import('@storybook/react').Preview } */
 const preview = {
